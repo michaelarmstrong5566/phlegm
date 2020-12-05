f4a13674b09dc3427293c4337568459db77153da
michaelarmstrong5566 <mcarmstrong@gmail.com>
2020-12-04 21:31:18 -0800

    deleted script.js and syle.js

--- script.js
+++
@@ -1,9 +0,0 @@
-let h1 = document.getElementsByTagName("h1")[0];
-console.log(h1)
-
-h1.addEventListener("mouseover", myFunction);
-
-function myFunction() {
-  document.getElementById("demo")
-  console.log("moused over");
-}
