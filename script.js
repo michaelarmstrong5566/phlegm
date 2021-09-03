edbd87d4ac1ea177354e8860493306222a02c66c
unknown <mcarmstrong@gmail.com>
2021-09-02 22:23:10 -0700

    button added

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function ratio(){
   let fvc = form[2];
   console.log(fvc.value);
 
+  let ratio = parseFloat(fev1.value)/parseFloat(fvc.value);
+  //console.log(typeof ratio);
+  console.log(ratio);
+
   document.getElementById("ratio").innerHTML = "FEV1/FVC ratio = " + (fev1.value/fvc.value).toFixed(2);
 
 }
